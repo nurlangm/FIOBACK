e05e76c8ab477e7c3b6b59e141743808064406b1
unknown <you@example.com>
2022-01-11 21:54:30 +0400

    task

---
+++ Fiorello/Controllers/FlowerController.cs
@@ -0,0 +1,25 @@
+﻿using Fiorello.DAL;
+using Fiorello.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiorello.Controllers
+{
+    public class FlowerController : Controller
+    {
+        private readonly AppDbContext _context;
+        public FlowerController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Details(int id)
+        {
+            Flower flower = _context.Flowers.Include(f=>f.Campaigns).Include(f=>f.FlowerCategories).ThenInclude(fc=>fc.Category).Include(f=>f.FlowerTags).ThenInclude(ft=>ft.Tag).Include(f=>f.FlowerImages).FirstOrDefault(f => f.Id == id);
+            return View(flower);
+        }
+    }
+}
